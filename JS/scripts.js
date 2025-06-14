62b096778b3eaadf1869f445fa492fbe269ef6f1
vipin jaiswal <vipi78040@gmail.com>
2025-06-15 01:22:08 +0530

    change the repo

---
+++ JS/scripts.js
@@ -0,0 +1,10 @@
+
+function handleMenu() {
+      const nav = document.getElementById("navbar");
+      const menu = document.getElementById("mobile-menu");
+
+      nav.classList.toggle("hidden");
+      menu.classList.toggle("hidden");
+    }
+
+    
