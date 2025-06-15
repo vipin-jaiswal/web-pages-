bd180861946178b8112e28476d5efd3355208418
vipin jaiswal <vipi78040@gmail.com>
2025-06-15 17:37:10 +0530

    Adding text and buttons in the main container

--- dist/scripts.js
+++ dist/scripts.js
@@ -2,9 +2,11 @@ const config = require('../tailwind.config.js');
 function handleMenu() {
       const nav = document.getElementById("navbar");
       const menu = document.getElementById("mobile-menu");
+      const main = document.getElementById("main")
 
       nav.classList.toggle("hidden");
       menu.classList.toggle("hidden");
+      main.classList.toggle("hidden");
     }
 
     
