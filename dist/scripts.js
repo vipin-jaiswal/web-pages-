1726dcf2d667b1f7bc9f0b292049040905629701
vipin jaiswal <vipi78040@gmail.com>
2025-06-16 01:44:25 +0530

    add companies-container

--- dist/scripts.js
+++ dist/scripts.js
@@ -8,5 +8,20 @@ function handleMenu() {
       menu.classList.toggle("hidden");
       main.classList.toggle("hidden");
     }
+    function setIntersectionObserver(element, isLTR, speed){
+      const intersetioncallback=(entris)=>{
+        const isIntersecting=Entries[0].isIntersecting;
+        console.log(element,isIntersecting)
+      }
+      const intersectionObserver= new IntersectionObserver(intersetioncallback)
+      intersectionObserver.unobserve(element);
+    }
+
+    const line1=document.getElementById('line1');
+    const line2=document.getElementById('line2');
+    const line3=document.getElementById('line3');
+    setIntersectionObserver(line1,true,0.15);
+    setIntersectionObserver(line2,false,0.15);
+    setIntersectionObserver(line3,true,0.15);
 
     
